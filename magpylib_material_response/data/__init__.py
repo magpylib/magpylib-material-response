6c1471cd1c9fa2b03633d14c4ff8d48d7509df5c
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2023-05-07 17:23:36 +0200

    remove extension

--- magpylib_material_response/data/__init__.py
+++ magpylib_material_response/data/__init__.py
@@ -6,7 +6,10 @@ Built-in datasets for demonstration, educational and test purposes.
 def get_dataset(name):
     import json
     import os
+    from pathlib import Path
 
+    stem = Path(name).stem
+    name = stem + ".json"
     file = os.path.join(
         os.path.dirname(os.path.dirname(__file__)),
         "package_data",
