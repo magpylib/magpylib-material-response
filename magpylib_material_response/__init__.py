b74290507eccc27d07ceb7ac6b7a05c590566e56
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2023-05-07 17:27:43 +0200

    bump version

--- magpylib_material_response/__init__.py
+++ magpylib_material_response/__init__.py
@@ -2,4 +2,4 @@
 analysis for soft materials and demagnetization of hard magnets. Leveraging the Method
 of Moments, it calculates magnetic material response with high precision."""
 
-__version__ = "0.1.0a4"
+__version__ = "0.1.0a5"
