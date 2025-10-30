6877a55795a99756e1035ddfb88872d06466bf12
Alexandre Boisselet <alexabois@gmail.com>
2025-10-30 21:49:06 +0100

    Improved documentation theme and layout  (#29)
    * move to pydata_sphinx_theme
    
    * feat: add full-width CSS and examples documentation
    
    * style: pre-commit fixes
    
    * update changelog
    
    * style: pre-commit fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -28,24 +28,42 @@ exclude_patterns = [
     ".venv",
 ]
 
-html_theme = "furo"
+html_theme = "pydata_sphinx_theme"
 
 html_theme_options: dict[str, Any] = {
-    "footer_icons": [
+    # "announcement": announcement,
+    "logo": {
+        "text": "Magpylib-Material-Response",
+        "image_light": "_static/images/magpylib_logo.png",
+        "image_dark": "_static/images/magpylib_logo.png",
+    },
+    "header_links_before_dropdown": 4,
+    "show_version_warning_banner": True,
+    "navbar_align": "content",  # [left, content, right] For testing that the navbar items align properly
+    "navbar_center": ["navbar-nav"],
+    "check_switcher": True,
+    "icon_links": [
         {
             "name": "GitHub",
             "url": "https://github.com/magpylib/magpylib-material-response",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
-            "class": "",
+            "icon": "https://img.shields.io/github/stars/magpylib/magpylib-material-response?style=social",
+            "type": "url",
         },
     ],
-    "source_repository": "https://github.com/magpylib/magpylib-material-response",
-    "source_branch": "main",
-    "source_directory": "docs/",
+    "navigation_with_keys": False,
+    "footer_start": ["copyright"],
+    "footer_end": [],
+    "use_edit_page_button": True,
+    "navigation_depth": 3,
+    "collapse_navigation": False,
+}
+
+html_context = {
+    # "github_url": "https://github.com", # or your GitHub Enterprise site
+    "github_user": "magpylib",
+    "github_repo": "magpylib-material-response",
+    "github_version": "main",
+    "doc_path": "docs/",
 }
 
 myst_enable_extensions = [
@@ -74,3 +92,11 @@ nitpick_ignore = [
 always_document_param_types = True
 
 suppress_warnings = ["mystnb.unknown_mime_type"]
+
+html_js_files = [
+    "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js",
+]
+
+# Static files (CSS/JS)
+html_static_path = ["_static"]
+html_css_files = ["fullwidth.css"]
---
+++ docs/examples/index.md
@@ -0,0 +1,11 @@
+# Examples
+
+This section contains examples demonstrating the use of
+Magpylib-Material-Response.
+
+```{toctree}
+:maxdepth: 1
+:glob:
+
+*
+```
